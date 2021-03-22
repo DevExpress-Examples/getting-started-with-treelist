9bdada21b9c8704a85fad9eae0e9de9853063cbe
LexDX <42230789+LexDX@users.noreply.github.com>
2021-03-22 19:26:52 +0300

    fix imports

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { DxTreeListModule } from 'devextreme-angular';
+import { DxTreeListModule } from 'devextreme-angular/ui/tree-list';
 
 @NgModule({
   declarations: [
